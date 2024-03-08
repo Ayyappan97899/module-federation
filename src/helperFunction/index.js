a8166c276055a6f179c5dda19568642bd06ff548
ayyappan97899 <ayyappan97899@gmail.com>
2024-03-08 17:09:08 +0530

    getArrayOfKey func added

--- src/helperFunction/index.js
+++ src/helperFunction/index.js
@@ -1,3 +1,6 @@
 // reverse str
 
 export const reverseString = (str) => str?.split("")?.reverse()?.join("");
+
+export const getArrayOfKey = (array, key = "") =>
+  array?.map((data) => data?.[key]);
